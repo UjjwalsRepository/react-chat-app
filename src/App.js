dc76bb8a230dc86a8723e7c62dfdc9f3c0d92da1
UjjwalsRepository <99465389+UjjwalsRepository@users.noreply.github.com>
2023-01-16 12:31:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,13 @@ import Login from './components/Login';
 import './App.css';
 
 const App=()=>{
-   //if(!localStorage.getItem('userName')) return <Login/>
+   
     return(
         <ChatEngine
         height="100vh"
         projectID="7f528c4b-9067-408f-9073-11f87bd338e8"
         userName="User2"
         userSecret="123456" 
-        // userName={localStorage.getItem('userName')}
-        // userSecret={localStorage.getItem('password')}
         renderChatFeed={(chatAppProps)=><ChatFeed {...chatAppProps}/>}
         />
     );
